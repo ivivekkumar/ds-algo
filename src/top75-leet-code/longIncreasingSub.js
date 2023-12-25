fbd5806f341a45871533fac25db41b4723eaad9e
Vivek Kumar <ivivekkumar01@gmail.com>
2023-12-25 15:10:26 +0530

    Corrected lis

--- src/top75-leet-code/longIncreasingSub.js
+++ src/top75-leet-code/longIncreasingSub.js
@@ -5,32 +5,28 @@
 // Below solution returns the length of lis in linear time.
 const longIncSub = (arr) => {
     let dp = new Array(arr.length).fill(1);
-  
+
     for (let i = 1; i < arr.length; i++) {
-      if (typeof arr[i] === 'number' && typeof arr[i - 1] === 'number') {
         if (arr[i] > arr[i - 1]) {
-          dp[i] = dp[i - 1] + 1;
-        } else {  // Corrected logic: reset for non-increasing numerical pairs
-          dp[i] = 1;
+            dp[i] = dp[i - 1] + 1;
+        } else {
+            dp[i] = 1;
         }
-      } else {
-        dp[i] = 1;  // Reset for non-numerical elements
-      }
     }
-  
+
     let max = dp[0];
     for (elem of dp) {
-      if (elem > max) {
-        max = elem;
-      }
+        if (elem > max) {
+            max = elem;
+        }
     }
-  
+
     return max;
-  };
-  
-  const sequence = [2,12,4,5,5,108,12];
-  console.log(longIncSub(sequence)); // Output: 2
-  
+};
+
+const sequence = [2, 12, 4, 5, 5, 108, 12];
+console.log(longIncSub(sequence)); // Output: 2
+
 // Below solution returns the length in case of mixed array aswell. Time Complexity: O(n^2).
 
 const longIncSubGenralised = (arr) => {
